2e7ac2a4650402ac785036cfa050d882fe9a3a69
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-09-19 01:32:09 -0300

    se registro el srvicio orm

---
+++ src/BaseVideoArte/Entidades/Artista.php
@@ -0,0 +1,50 @@
+<?php
+// src/MyWebsite/Entity/Article.php
+namespace BaseVideoArte\Entidades;
+/**
+ * @Entity
+ */
+class Artista {
+
+    /** 
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+
+    /** @Column(type="text") */
+    private $nombre;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Artista
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
--- src/app.php
+++ src/app.php
@@ -6,10 +6,49 @@ use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 
+//DOCTRINE
+use \Doctrine\Common\Cache\ApcCache;
+use \Doctrine\Common\Cache\ArrayCache;
+
+
+
+
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
+
+
+
+// Register Doctrine DBAL
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options'    => array(
+    'driver'        => 'pdo_mysql',
+    'host'          => 'localhost',
+    'dbname'        => 'test_framework',
+    'user'          => 'jose',
+    'password'      => 'jose',
+    'charset'       => 'utf8',
+    'driverOptions' => array(1002 => 'SET NAMES utf8',),
+  ),
+));
+
+
+// Register Doctrine ORM
+$app->register(new Nutwerk\Provider\DoctrineORMServiceProvider(), array(
+    'db.orm.proxies_dir'           => __DIR__ . '/cache/doctrine/proxy',
+    'db.orm.proxies_namespace'     => 'DoctrineProxy',
+    'db.orm.cache'                 => 
+        !$app['debug'] && extension_loaded('apc') ? new ApcCache() : new ArrayCache(),
+    'db.orm.auto_generate_proxies' => true,
+    'db.orm.entities'              => array(array(
+        'type'      => 'annotation',       // entity definition 
+        'path'      => __DIR__ . '/BaseVideoArte/Entidades',   // path to your entity classes
+      
+        'namespace' => 'BaseVideoArte\Entidades', // your classes namespace
+    )),
+));
 
 
 // $app->register(new TwigServiceProvider(), array(

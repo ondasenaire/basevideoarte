30d3f82c3a58bf2e399d4e79e0b4d182e1f7ca3b
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-01 20:47:43 -0300

    registrado servicio form y translations

--- src/app.php
+++ src/app.php
@@ -5,79 +5,59 @@ use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
-require_once __DIR__.'/../config/config.db';
+use Silex\Provider\FormServiceProvider;
+require_once __DIR__ . '/../config/config.db';
 //DOCTRINE
 use \Doctrine\Common\Cache\ApcCache;
 use \Doctrine\Common\Cache\ArrayCache;
 use BaseVideoArte\Controller\VideoArteController;
-//use BaseVideoArte\Controller\VideoArteController; 
-
+//use BaseVideoArte\Controller\VideoArteController;
 
 $db = new Config();
 
-
 $app = new Application();
-$app->register(new UrlGeneratorServiceProvider());
-$app->register(new ValidatorServiceProvider());
-$app->register(new ServiceControllerServiceProvider());
+$app -> register(new UrlGeneratorServiceProvider());
+$app -> register(new ValidatorServiceProvider());
+$app -> register(new ServiceControllerServiceProvider());
 
-//registro controlador
-// $app['video-arte.controller'] = $app->share( function (){
-	// return new VideoArteController($app);
-// }
-// );
+// Form
+$app->register(new FormServiceProvider());
+// translator
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+      'locale' => 'es',
+      'translation.class_path' =>  __DIR__ . '/../vendor/symfony/src',
+      'translator.messages' => array()
+)) ;
 
-$app['videoarte.controller'] = $app->share( function (){
+
+
+
+$app['videoarte.controller'] = $app -> share(function() {
 	return new VideoArteController($app);
-}
-);
-
+});
 
 // Register Doctrine DBAL
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options'    => array(
-    'driver'        => 'pdo_mysql',
-    'host'          => $db->getHost(),
-    'dbname'        => $db->getBD(),
-    'user'          => $db->getUsuario(),
-    'password'      => $db->getPass(),
-    'charset'       => 'utf8',
-    'driverOptions' => array(1002 => 'SET NAMES utf8',),
-  ),
-));
-
+$app -> register(new Silex\Provider\DoctrineServiceProvider(), array('db.options' => array('driver' => 'pdo_mysql', 'host' => $db -> getHost(), 'dbname' => $db -> getBD(), 'user' => $db -> getUsuario(), 'password' => $db -> getPass(), 'charset' => 'utf8', 'driverOptions' => array(1002 => 'SET NAMES utf8', ), ), ));
 
 // Register Doctrine ORM
-$app->register(new Nutwerk\Provider\DoctrineORMServiceProvider(), array(
-    'db.orm.proxies_dir'           => __DIR__ . '/../cache/doctrine/proxy',
-    'db.orm.proxies_namespace'     => 'DoctrineProxy',
-    'db.orm.cache'                 => 
-        !$app['debug'] && extension_loaded('apc') ? new ApcCache() : new ArrayCache(),
-    'db.orm.auto_generate_proxies' => true,
-    'db.orm.entities'              => array(array(
-        'type'      => 'annotation',       // entity definition 
-        'path'      => __DIR__ . '/BaseVideoArte/Entidades',   // path to your entity classes
-      
-        'namespace' => 'BaseVideoArte\Entidades', // your classes namespace
-    )),
-));
+$app -> register(new Nutwerk\Provider\DoctrineORMServiceProvider(), array('db.orm.proxies_dir' => __DIR__ . '/../cache/doctrine/proxy', 'db.orm.proxies_namespace' => 'DoctrineProxy', 'db.orm.cache' => !$app['debug'] && extension_loaded('apc') ? new ApcCache() : new ArrayCache(), 'db.orm.auto_generate_proxies' => true, 'db.orm.entities' => array( array('type' => 'annotation', // entity definition
+'path' => __DIR__ . '/BaseVideoArte/Entidades', // path to your entity classes
 
+'namespace' => 'BaseVideoArte\Entidades', // your classes namespace
+)), ));
 
 // $app->register(new TwigServiceProvider(), array(
-    // 'twig.path'    => array(__DIR__.'/../src/views'),
-    // 'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
-//     
+// 'twig.path'    => array(__DIR__.'/../src/views'),
+// 'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
+//
 // ));
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/../src/views',
-    'twig.class_path' => __DIR__ . '/../vendor/twig/lib',
-));
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-    // add custom globals, filters, tags, ...
+$app -> register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__ . '/../src/views', 'twig.class_path' => __DIR__ . '/../vendor/twig/lib', ));
+$app['twig'] = $app -> share($app -> extend('twig', function($twig, $app) {
+	// add custom globals, filters, tags, ...
 
-    return $twig;
+	return $twig;
 }));
 
 return $app;

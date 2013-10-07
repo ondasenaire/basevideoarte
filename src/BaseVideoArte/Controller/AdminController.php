b732532297e1ce00e9199d9d44004cea70590aab
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-07 12:10:01 -0300

    funciona el form multiple

--- src/BaseVideoArte/Controller/AdminController.php
+++ src/BaseVideoArte/Controller/AdminController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use BaseVideoArte\Entidades\Persona;
 use BaseVideoArte\Form\Admin\PersonaType;
 
-
 use BaseVideoArte\Entidades\Metadato;
 use BaseVideoArte\Form\Admin\MetadatoType;
 
@@ -24,81 +23,68 @@ class AdminController {
 	public function listarPersonasAction() {
 	}
 
-	public function nuevaPersonaAction(Application $app,Request $request) {
+	public function nuevaPersonaAction(Application $app, Request $request) {
 
-	//	$persona = new Persona();
+		//	$persona = new Persona();
 		/*
 		 * ESTO ES POCO PRACTICO PERO .... con Silex esta complicado
-		 */	 
-		$persona = array (
-			'nombre' => 'introduzca los nombres',
-			'apellido' => 'apellido de la persona',
-			'pais' => '',
-			'data' => '',
-			'inicio' => '',
-			'sexo' => '',
-			'web' => ''	,
-			'tipo' => '',
-			'mostrar' => true
-				
-		); 		
-		
+		 */
+		$persona = array('nombre' => 'introduzca los nombres', 'apellido' => 'apellido de la persona', 'pais' => '', 'data' => '', 'inicio' => '', 'sexo' => '', 'web' => '', 'tipo' => '', 'mostrar' => true);
+
 		/* Campos 'entity' */
-		
+
 		$consulta = $app['db.orm.em'] -> createQuery('SELECT p FROM BaseVideoArte\Entidades\Pais p');
 		$paises = $consulta -> getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
-		
+
 		$con = $app['db.orm.em'] -> createQuery('SELECT t FROM BaseVideoArte\Entidades\TipoDePersona t');
-		$tipos = $con-> getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
-		
+		$tipos = $con -> getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+
 		foreach ($paises as $pais) {
 			$p[$pais['id']] = $pais['pais'];
 		}
-		
+
 		foreach ($tipos as $tipo) {
 			$t[$tipo['id']] = $tipo['tipo'];
 		}
-		
-		$fp = new PersonaType();
-		$fp->setOpcionesTipo($t);
-		$fp->setOpcionesPais($p);
-		
-		
-		$form = $app["form.factory"] -> create($fp,$persona);
-			
-		//...PROCESAR PETICION
-		if($request->isMethod('POST') ){
-					
-			$form->bind($request);
-			
-			if($form->isValid( )){
-				$persona = $form->getData();
-				/***/
-				
-				$nuevaPersona = new Persona();
-				$nuevaPersona->setNombre($persona['nombre']);
-				$nuevaPersona->setApellido($persona['apellido']);
-				$nuevaPersona->setData($persona['data']);
-				$nuevaPersona->setInicio($persona['inicio']);
-				$nuevaPersona->setSexo($persona['sexo']);
-				$nuevaPersona->setWeb($persona['web']);
-				//pais				
-				$nuevaPersona->setPais($app['db.orm.em'] ->getRepository('BaseVideoArte\Entidades\Pais')->findOneById($persona['pais']));
-				//	
-				
-				//mañana hacer esto
-				$nuevaPersona->setTipo(  $app['db.orm.em'] ->getRepository('BaseVideoArte\Entidades\TipoDePersona')->findOneById($persona['tipo']) );
-				$nuevaPersona->setMostrar($persona['mostrar']);
-							
-				//echo  $nuevaPersona->getPais()->getPais();
-				$app['db.orm.em'] ->persist($nuevaPersona);
-				$app['db.orm.em'] ->flush();
 
-			}else{
+		$fp = new PersonaType();
+		$fp -> setOpcionesTipo($t);
+		$fp -> setOpcionesPais($p);
+
+		$form = $app["form.factory"] -> create($fp, $persona);
+
+		//...PROCESAR PETICION
+		if ($request -> isMethod('POST')) {
+
+			$form -> bind($request);
+
+			if ($form -> isValid()) {
+				$persona = $form -> getData();
+				/***/
+
+				$nuevaPersona = new Persona();
+				$nuevaPersona -> setNombre($persona['nombre']);
+				$nuevaPersona -> setApellido($persona['apellido']);
+				$nuevaPersona -> setData($persona['data']);
+				$nuevaPersona -> setInicio($persona['inicio']);
+				$nuevaPersona -> setSexo($persona['sexo']);
+				$nuevaPersona -> setWeb($persona['web']);
+				//pais
+				$nuevaPersona -> setPais($app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Pais') -> findOneById($persona['pais']));
+				//
+
+				//mañana hacer esto
+				$nuevaPersona -> setTipo($app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\TipoDePersona') -> findOneById($persona['tipo']));
+				$nuevaPersona -> setMostrar($persona['mostrar']);
+
+				//echo  $nuevaPersona->getPais()->getPais();
+				$app['db.orm.em'] -> persist($nuevaPersona);
+				$app['db.orm.em'] -> flush();
+
+			} else {
 				echo 'No Valido ';
 			}
 		}
-		
 
 		return $app['twig'] -> render('/Admin/nueva_persona.twig.html', array('form' => $form -> createView()));
 	}
@@ -109,35 +95,40 @@ class AdminController {
 	public function editarPersonasAction() {
 	}
 
+	public function metadatoAction(Application $app, Request $request) {
 
-	public function metadatoAction( Application $app ){
-		
-		$coll = array('metadatos' => array(),
-				      'titulo'=>'gf');
-		
+		$coll = array('metadatos' => array(), 'titulo' => 'gf');
+
 		$m = new Metadato();
-		$m2 = new Metadato();
-		
-		$m->setMetadato(" estra");
-		$m2->setMetadato(" estra2");
-		
-		//$coll = array( 'metadatos' => array('1'=>'csaacsd','1'=>'ceddrtd') );
-		
-		$coll['metadatos'][] = $m;
-		$coll['metadatos'][] = $m2;
-				
+		//$m2 = new Metadato();
+		//$m->setMetadato(" estra");
+		//$m2->setMetadato(" estra2");
+		//$coll['metadatos'][] = $m;
+		//$coll['metadatos'][] = $m2;
+
 		$mt = new MetadatoType();
-		
-		
-		$mt->setOpcionesTipo($m->getTipos());
-		
+		$mt -> setOpcionesTipo($m -> getTipos());
 		$multiple = new MultipleType();
-		$multiple->setOpciones($mt);
-		
-		$form = $app["form.factory"] -> create($multiple,$coll);		
-		return $app['twig'] -> render('/Admin/pruebas.twig.html', array('meta' => $m ,
-																		'form' => $form->createView()					
-		));
-		
+		$multiple -> setOpciones($mt);
+		$form = $app["form.factory"] -> create($multiple, $coll);
+
+		if ($request -> isMethod('POST')) {
+			$form -> bind($request);
+
+			if ($form -> isValid()) {
+				$coll = $form -> getData();
+
+				//echo $coll['metadatos']['meta2'] -> getMetadato();
+				//print_r($coll);
+
+				foreach ( $coll['metadatos'] as $meta) {
+					echo ' '.$meta->getMetadato();
+				}
+			}
+		}
+
+		return $app['twig'] -> render('/Admin/pruebas.twig.html', array('meta' => $m, 'form' => $form -> createView()));
+
 	}
+
 }

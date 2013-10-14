8e0145d45a2f73ece4625808efc3b3e22dab31e1
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-14 16:02:40 -0300

    clase fachada para manejar las instrucciones

---
+++ src/BaseVideoArte/util/Manejador/ManejadorFachada.php
@@ -0,0 +1,13 @@
+<?php
+namespace BaseVideoArte\Util\Manejador;
+
+use BaseVideoArte\Entidades\Evento;
+use BaseVideoArte\Entidades\Persona;
+use BaseVideoArte\Entidades\Obra;
+use BaseVideoArte\Entidades\Metadato;
+
+class ManejadorFachada{
+	
+	
+	
+}

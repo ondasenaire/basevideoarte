060c8faccafd21f40022bca3afb292f0c96a0973
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-14 23:04:24 -0300

    filtra por inicial del apellido de la persona

--- src/BaseVideoArte/Controller/VideoArteController.php
+++ src/BaseVideoArte/Controller/VideoArteController.php
@@ -9,29 +9,51 @@ class VideoArteController {
 		//return $app['twig']->render('/inicio.html.twig', array());
 		return new Response('<html><body>Hello ' . $h . '!</body></html>');
 	}
+
 	//----------------------------------------------------------------
 	// PERSONAS
-	public function listarPersonasAction(Application $app,$filtro,$valor){
-		$qb = $app['db.orm.em']->createQueryBuilder();		
-		$qb->select('p.id','p.nombre','p.apellido')
-		   ->from('BaseVideoArte\Entidades\Persona','p');		   
-		 $consulta = $qb->getQuery();
-		 $personas = $consulta->getResult();
-		 echo $filtro;
-		 echo $valor;
-		return $app['twig'] -> render('/personas.html.twig', array('lista_personas' => $personas));
-	} 
+	public function listarPersonasAction(Application $app, $filtro, $valor) {
 
-	public function mostrarPersonaAction(Application $app, $persona){
+		if ($filtro != null) {
+			if ($filtro == 'abc' && $valor != null) {
+				echo 'filtra por abecedario con la letra '.$valor;
+				
+			//	$resultado =  $app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Persona')
+				//								->findBy(array('apellido'=>'SUBSTRING(apellido, 1, 1) = '.$valor));
+					$q =$app['db.orm.em'] -> createQueryBuilder();
+       				$q ->select('p.id', 'p.nombre', 'p.apellido')
+      				 ->from('BaseVideoArte\Entidades\Persona',' p')
+        			 ->where("SUBSTRING(p.apellido, 1, 1) = '$valor'");		
+					$consulta = $q->getQuery(); 					
+					$resultado = $consulta->getResult();								
+				
+			}
+			if ($filtro == 'pais' && $valor != null) {
+				echo 'filtra por pais';
+			}
+
+		}
+	
+	//	$qb = $app['db.orm.em'] -> createQueryBuilder();
+	//	$qb -> select('p.id', 'p.nombre', 'p.apellido') -> from('BaseVideoArte\Entidades\Persona', 'p');
+	//	$consulta = $qb -> getQuery();
+	//	$personas = $consulta -> getResult();
+	//	echo $filtro;
+		//echo $valor;
+		return $app['twig'] -> render('/personas.html.twig', array('lista_personas' => $resultado));
+	}
+
+	public function mostrarPersonaAction(Application $app, $persona) {
 		$repoPersonas = $app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Persona');
-		$persona = $repoPersonas->findOneById($persona);
+		$persona = $repoPersonas -> findOneById($persona);
 		return $app['twig'] -> render('/persona.twig.html', array('persona' => $persona));
-	} 
+	}
+
 	//----------------------------------------------------------------
 	//OBRAS
 	public function listarObrasAction(Application $app) {
-		$repositorioObras = $app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Obra');					 
-		$obras = $repositorioObras->findAll();
+		$repositorioObras = $app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Obra');
+		$obras = $repositorioObras -> findAll();
 		return $app['twig'] -> render('/obras.html.twig', array('lista_obras' => $obras));
 	}
 
@@ -44,14 +66,14 @@ class VideoArteController {
 	//----------------------------------------------------------------
 	//EVENTOS
 	public function listarEventosAction(Application $app) {
-		 $consulta = $app['db.orm.em']->createQuery('SELECT e.id,e.nombre,e.anho, SUBSTRING(e.info,1,300) AS info  FROM BaseVideoArte\Entidades\Evento e' );
-		 $eventos = $consulta->getResult();
-		 return $app['twig'] -> render('/eventos.twig.html', array('eventos' => $eventos ));
+		$consulta = $app['db.orm.em'] -> createQuery('SELECT e.id,e.nombre,e.anho, SUBSTRING(e.info,1,300) AS info  FROM BaseVideoArte\Entidades\Evento e');
+		$eventos = $consulta -> getResult();
+		return $app['twig'] -> render('/eventos.twig.html', array('eventos' => $eventos));
 	}
 
 	public function mostrarEventoAction(Application $app, $evento) {
 		$repoEventos = $app['db.orm.em'] -> getRepository('BaseVideoArte\Entidades\Evento');
-		$evento = $repoEventos->findOneById($evento);		
+		$evento = $repoEventos -> findOneById($evento);
 		return $app['twig'] -> render('/evento.html.twig', array('evento' => $evento));
 	}
 

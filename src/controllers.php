a036421e0c1716111fa4b6a81652366b4d7e4ba7
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-18 23:43:59 -0300

    un poco de limpieza en el codigo. Controlador especifico para hacer pruebas con datos

--- src/controllers.php
+++ src/controllers.php
@@ -27,6 +27,8 @@ $app->get('/personas/{filtro}/{valor}',    'BaseVideoArte\Controller\VideoArteCo
 $app->get('/persona/{persona}',    'BaseVideoArte\Controller\VideoArteController::mostrarPersonaAction')
 	->bind('persona')
 	->assert('persona','\d+');
+	
+	
 // OBRAS
 $app->get('/obras',    'BaseVideoArte\Controller\VideoArteController::listarObrasAction')->bind('obras');
 $app->get('/obras/{obra}',    'BaseVideoArte\Controller\VideoArteController::mostrarObraAction')->bind('obra');
@@ -40,20 +42,25 @@ $app->get('/eventos/{evento}','BaseVideoArte\Controller\VideoArteController::mos
 $app->get('/admin', 'BaseVideoArte\Controller\AdminController::indexAction')->bind('admin');
 $app->get('/admin/personas', 'BaseVideoArte\Controller\AdminController::editarPersonasAction')->bind('admin_personas');
 $app->match('/admin/nueva_persona', 'BaseVideoArte\Controller\AdminController::nuevaPersonaAction')->bind('admin_nueva_persona');
-$app->match('/admin/pruebas', 'BaseVideoArte\Controller\AdminController::metadatoAction')->bind('pruebas');
-$app->match('/admin/reset', 'BaseVideoArte\Controller\AdminController::ingresarDatosAction')->bind('reset');
-$app->match('/admin/pruebas_get_metadatos', 'BaseVideoArte\Controller\AdminController::recuperarMetadataAction')->bind('get_metadatos');
+
 //BUSCADOR
 $app->get('/busqueda', function () use ($app) {
     return $app['twig']->render('/busqueda.html.twig', array());
 })
 ->bind('busqueda')
 ;
-//---------------------------------------------------------------------
+//--------------MANTENIMIENTO-----------------------------------------------------
 
 $app->get('/admin/cargar', 'BaseVideoArte\Controller\CargaDatosController::cargarDatosAction')->bind('cargar_datos');
-$app->get('/admin/colab', 'BaseVideoArte\Controller\AdminController::colaboradorAction')->bind('colab');
+
+
+//-------------PRUEBAS-----
+$app->get('/test/colab', 'BaseVideoArte\Controller\TestController::colaboradorAction')->bind('colab');
 $app->get('/admin/tipo', 'BaseVideoArte\Controller\CargaDatosController::tipoAction')->bind('tipo');
+$app->match('/test/pruebas', 'BaseVideoArte\Controller\TestController::metadatoAction')->bind('pruebas');
+$app->match('/test/reset', 'BaseVideoArte\Controller\TestController::ingresarDatosAction')->bind('reset');
+$app->match('/test/pruebas_get_metadatos', 'BaseVideoArte\Controller\TestController::recuperarMetadataAction')->bind('get_metadatos');
+
 //---------------------------------------------------------------------
 //contacto y "sobre el proyecto"
 $app->get('/contacto', function () use ($app) {

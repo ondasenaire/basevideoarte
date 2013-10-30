41f3c096fd56ad54048727ed04fc6a689a3f8082
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-10-30 12:04:57 -0300

    ! CargaDatos permite dividir los datos en archivos

--- src/BaseVideoArte/util/Carga/CargadorDatos.php
+++ src/BaseVideoArte/util/Carga/CargadorDatos.php
@@ -21,8 +21,10 @@ class CargadorDatos {
 	private $personas;
 	private $obras;
 	private $eventos;
+	private $datos;
 
 	public function cargar() {
+		$this -> datos = '/datos';
 		$this -> procesarPaises();
 		$this -> procesarTipos();
 		$this -> procesarFormatos();
@@ -30,7 +32,7 @@ class CargadorDatos {
 
 		$this -> procesarObras();
 		$this -> procesarPersonas();
-
+		$this -> buscarArchivos('obras');
 	}
 
 	public function asociar() {
@@ -48,9 +50,8 @@ class CargadorDatos {
 		}
 
 		foreach ($this->generos as $genero) {
-		$app['db.orm.em'] -> persist($genero);
+			$app['db.orm.em'] -> persist($genero);
 		}
-		
 
 		foreach ($this->tipos as $tipo) {
 			$app['db.orm.em'] -> persist($tipo);
@@ -60,9 +61,9 @@ class CargadorDatos {
 		// $app['db.orm.em'] -> persist($medio);
 		// }
 		//
-	//	foreach ($this->metadatos as $metadato) {
+		//	foreach ($this->metadatos as $metadato) {
 		//$app['db.orm.em'] -> persist($metadato);
-	//	}
+		//	}
 		//
 		//----------------------
 
@@ -92,91 +93,128 @@ class CargadorDatos {
 		return $datos;
 	}
 
-	public function procesarPaises() {
+	public function buscarArchivos($tipo) {
 
-		$lista_paises = $this -> json("/paises.json");
+		$finder = new \Symfony\Component\Finder\Finder();
+		$finder -> files() -> in(__DIR__ . $this -> datos) -> name($tipo . '*');
+		//	echo '<br> en la carpeta datos hay: <br> ';
+		// foreach ($finder as $file) {
+		//
+		// echo $file -> getRelativePathname() . "<br>";
+		// }
+		//echo '<br>'.iterator_count($finder).'<br>';
+		//print_r( iterator_to_array($finder) );
+
+		return iterator_to_array($finder);
+	}
+
+	//--------------
+	/*	Algoritmo Procesar  (con finder, sin finder desde paso 3)
+	 * 1. busco archivos correspondientes al la entidad (personas,obras,paises)
+	 * 2. recorro la lista de archivos y por cada uno encontrado
+	 * 3. abro el archivo .json y vuelco la info en un array ( json_decode mediante metodo json)
+	 * 4. recorro el array json y por cada elemento creo una instancia de la entidad correspondiente
+	 * 5. asigno esa instancia al listado general
+	 */
+	/// -------------------
+	public function procesarPaises() {// va a haber un solo archivo paises, no tiene mucho sentido el finder
+
+		$lista_paises = $this -> json($this -> datos . "/paises.json");
 		foreach ($lista_paises as $clave => $pais) {
 			$this -> paises[$clave] = new Pais($pais['pais']);
 		}
 
 	}
 
-	public function procesarTipos() {
-		$lista_tipos = $this -> json("/tipos.json");
+	public function procesarTipos() {// idem paises
+		$lista_tipos = $this -> json($this -> datos . "/tipos.json");
 		foreach ($lista_tipos as $clave => $tipo) {
 			$this -> tipos[$clave] = new TipoDePersona($tipo['tipo']);
 		}
 	}
 
-	public function procesarFormatos() {
-		$lista_formatos = $this -> json("/formatos.json");
+	public function procesarFormatos() {// idem paises
+		$lista_formatos = $this -> json($this -> datos . "/formatos.json");
 		foreach ($lista_formatos as $clave => $formato) {
 			$this -> formatos[$clave] = new Formato($formato['formato']);
 		}
 	}
 
-	public function procesarGeneros() {
-		$lista_generos = $this -> json("/generos.json");
+	public function procesarGeneros() {// idem paises
+		$lista_generos = $this -> json($this -> datos . "/generos.json");
 		foreach ($lista_generos as $clave => $genero) {
 			$this -> generos[$clave] = new Genero($genero['genero']);
 		}
 	}
-
-
-
-
+	// PERSONAS, OBRAS Y EVENTOS
+	/*
+	 * el metodo procesar carga las obras, pais y metadados en Personas
+	 */   
 	public function procesarPersonas() {
-		$lista_personas = $this -> json("/personas.json");
-		foreach ($lista_personas as $clave => $persona) {
-			$p = new Persona($persona['nombre'], $persona['apellido'], $persona['data'], $persona['inicio'], $persona['web'], $persona['sexo'], $persona['mostrar']);
-			$p -> setPais($this -> paises[$persona['pais']]);
-			//;
-			foreach ($persona['tipo'] as $tipo) {
-				
-				$p -> addTipo($this -> tipos[$tipo]);
+		// debido a la cantidad de personar se usa el finder para organizar las personas en grupos
+		//finder
+		$finder = $this -> buscarArchivos('personas');
+		foreach ($finder as $archivo) {
+			//----- paso 3
+			$lista_personas = $this -> json($this -> datos .'/' . $archivo -> getRelativePathname());
+			foreach ($lista_personas as $clave => $persona) {
+				$p = new Persona($persona['nombre'], $persona['apellido'], $persona['data'], $persona['inicio'], $persona['web'], $persona['sexo'], $persona['mostrar']);
+				$p -> setPais($this -> paises[$persona['pais']]);
+				//proceso tipo;
+				foreach ($persona['tipo'] as $tipo) {
+
+					$p -> addTipo($this -> tipos[$tipo]);
+
+				}
+				//proceso las obras
+				foreach ($persona['obras'] as $obra) {
+
+					$p -> addObra($this -> obras[$obra]);
+
+				}
+				//metadatos persona
+				foreach ($persona['metadatos'] as $metadato) {
+					//busca el metadato y lo asocia
+					echo 'SOY UN METADATO PERSONA';
+					$met = new \BaseVideoArte\Entidades\MetadatoPersona($metadato['metadato'], $metadato['tipo']);
+					$p -> addMetadato($met);
+					$met -> setPersona($p);
+				}
+				$this -> personas[$clave] = $p;
 
 			}
-			foreach ($persona['obras'] as $obra) {
-				
-				$p -> addObra($this -> obras[$obra]);
 
-			}
-			//metadatos persona
-			foreach ($persona['metadatos'] as $metadato) {
-				//busca el metadato y lo asocia
-				echo 'SOY UN METADATO PERSONA';
-				$met = new \BaseVideoArte\Entidades\MetadatoPersona($metadato['metadato'],$metadato['tipo']);				
-				$p -> addMetadato($met);
-				$met->setPersona($p);
-			}
-			$this -> personas[$clave] = $p;
-
-		}
+		}//fin finder
 	}
 
 	public function procesarObras() {
-		$lista_obras = $this -> json("/obras.json");
-		foreach ($lista_obras as $clave => $obra) {
 
-			$o = new Obra($obra['titulo'], $obra['sinopsis'], $obra['anho'], $obra['duracion']);
-			//busca y agrega los formatos
-			foreach ($obra['formato'] as $formato) {
-				$o -> addFormato($this -> formatos[$formato]);
+		$finder = $this -> buscarArchivos('obras');
+		foreach ($finder as $archivo) {
+			//--
+			$lista_obras = $this -> json($this -> datos.'/' . $archivo -> getRelativePathname());
+			foreach ($lista_obras as $clave => $obra) {
+
+				$o = new Obra($obra['titulo'], $obra['sinopsis'], $obra['anho'], $obra['duracion']);
+				//busca y agrega los formatos
+				foreach ($obra['formato'] as $formato) {
+					$o -> addFormato($this -> formatos[$formato]);
+				}
+				//busca y agrega los generos
+				foreach ($obra['genero'] as $genero) {
+					$o -> addGenero($this -> generos[$genero]);
+
+				}
+
+				foreach ($obra['metadatos'] as $metadato) {
+					//busca el metadato y lo asocia
+					$met = new \BaseVideoArte\Entidades\MetadatoObra($metadato['metadato'], $metadato['tipo']);
+					$o -> addMetadato($met);
+					$met -> setObra($o);
+				}
+
+				$this -> obras[$clave] = $o;
 			}
-			//busca y agrega los generos
-			foreach ($obra['genero'] as $genero) {
-				$o -> addGenero($this -> generos[$genero]);
-				
-			}
-			
-			foreach ($obra['metadatos'] as $metadato) {
-				//busca el metadato y lo asocia
-				$met = new \BaseVideoArte\Entidades\MetadatoObra($metadato['metadato'],$metadato['tipo']);				
-				$o -> addMetadato($met);
-				$met->setObra($o);
-			}
-			
-			$this -> obras[$clave] = $o;
 		}
 	}
 

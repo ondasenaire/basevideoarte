e625c12a0c4cd100b1d9f7693b71b37dad7ed034
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-11-18 17:01:53 -0300

    buscar obras general

--- src/BaseVideoArte/Controller/BuscadorController.php
+++ src/BaseVideoArte/Controller/BuscadorController.php
@@ -13,46 +13,62 @@ class BuscadorController {
 
 	public function buscadoresAction(Application $app, Request $request) {
 
-//  FORM GENERAL
+		//  FORM GENERAL
 		$buscadorGeneral = new BuscadorGeneralType();
 		$form_general = $app["form.factory"] -> create($buscadorGeneral);
-//  FORM PERSONAS		
+		//  FORM PERSONAS
 		$buscadorPersonas = new BuscadorPersonasType();
 		$form_personas = $app["form.factory"] -> create($buscadorPersonas);
-//  FORM OBRAS
+		//  FORM OBRAS
 		$buscadorObras = new BuscadorObrasType();
 		$form_obras = $app["form.factory"] -> create($buscadorObras);
-//  FORM AVANZADo
+		//  FORM AVANZADo
 
-// FORM EVENTOS
+		// FORM EVENTOS
 
-//---------------
+		//---------------
+
+		$mensaje = '';
+		$respuesta = array('mensaje'=>' ', 'resultado' => array());
 		if ($request -> isMethod('POST')) {
 
 			if ($request -> request -> has('buscador_general')) {
-				echo 'buscador general';
+				//echo 'buscador general';
+				$respuesta = $this -> busquedaGeneral($app);
+				
 			}
 
 			if ($request -> request -> has('buscador_personas')) {
-				echo 'buscador personas';
+				//$respuesta['mensaje'] = 'hay una busqueda de personas que procesar';
 			}
-			
+
 			if ($request -> request -> has('buscador_obras')) {
-				echo 'buscador obras';
+				//$respuesta ['mensaje'] = 'hay una busqueda de obras que procesar';
 			}
 
 		}
 
-		return $app['twig'] -> render('/busqueda.twig.html', 
-				array('form_general' => $form_general -> createView(),
-				'form_personas' => $form_personas -> createView(),
-				'form_obras' => $form_obras -> createView(),
-				 'pagina_actual'=>'busqueda'  ));
+		return $app['twig'] -> render('/busqueda.twig.html', array('form_general' => $form_general -> createView(), 
+																	'form_personas' => $form_personas -> createView(), 
+																	'form_obras' => $form_obras -> createView(), 
+																	'pagina_actual' => 'busqueda', 
+																	'respuesta' => $respuesta) );
 
 	}
+//  CONSULTASSSSS
 
-	public function bucadorGeneral() {
+	public function busquedaGeneral(Application $app) {
+		// $q_obras = $app['db.orm.em'] -> createQueryBuilder();
+		// $q_obras -> select('obra') -> from('BaseVideoArte\Entidades\Obra', ' obra') -> where() -> orderBy('obra.titulo', 'ASC');
+		// $consulta = $q -> getQuery();
+		
+		
+		$query = $app['db.orm.em']->createQuery("SELECT obra.id, obra.titulo AS encabezado FROM BaseVideoArte\Entidades\Obra obra WHERE obra.titulo LIKE '%or%'");
+		$obras = $query->getResult();
+		
 
+		
+		return array('mensaje' => 'hay una busqueda general que procesar', 'resultado' => $obras);
 	}
 
 }

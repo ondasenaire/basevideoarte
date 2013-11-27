0b48dceb9a1b2a1f030b5be58a95cc67d01550e0
jose maria casanova <jmc.ondasenaire@gmail.com>
2013-11-26 22:30:54 -0300

    _token unico en gral

--- src/BaseVideoArte/Form/Buscador/BuscadorGeneralType.php
+++ src/BaseVideoArte/Form/Buscador/BuscadorGeneralType.php
@@ -5,7 +5,7 @@ namespace BaseVideoArte\Form\Buscador;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 
 class BuscadorGeneralType extends AbstractType {
@@ -15,10 +15,20 @@ class BuscadorGeneralType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder -> add("consulta", "text");
 		$builder -> add("buscar", "submit");
+		
 	}
 	
 
-	
+	   public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            // una clave única para ayudar generar el token
+            'intention'       => 'a',
+        ));
+    }
 		
 
 	public function getName() {
